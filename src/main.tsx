8591aac4275dec64ab7995e323347e2ddea4ab40
giorgijintcharadze <giorgijintcharadze7@gmail.com>
2026-07-13 01:48:35 +0400

    added : feature based structure and  implemented tanstack/query

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./App.css";
 import App from "./App.tsx";
+import { QueryProvider } from "./feature/query/QueryProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryProvider>
+      <App />
+    </QueryProvider>
   </StrictMode>,
 );
