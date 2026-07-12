a4ef27493cd1dc0d6eef324eb18b9ecb061f5bcf
giorgijintcharadze <giorgijintcharadze7@gmail.com>
2026-07-13 02:25:14 +0400

    added:  ESLint and Prettier configuration

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./App.css";
 import App from "./App.tsx";
-import { QueryProvider } from "./feature/query/QueryProvider.tsx";
+import { QueryProvider } from "./feature/lib/query/QueryProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
